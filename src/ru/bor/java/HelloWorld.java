c5a6478c2d2f845842bd98fd47a78f8e72eaf56f
Anton-Bor <grisha1987_06@tut.by>
2022-03-03 00:06:23 +0300

    Add line.

--- src/ru/bor/java/HelloWorld.java
+++ src/ru/bor/java/HelloWorld.java
@@ -3,5 +3,6 @@ package ru.bor.java;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("New line");
     }
 }
